8743f1f72d0799819d4837068eb1c50057f76ec3
Towsif Mahabub <147875322+Towsifsnm@users.noreply.github.com>
2024-09-19 01:11:08 +0600

    added partially theme

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log("hi dear")
+// console.log("hi dear")
+// step -1 : add event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+
+// s-2: get amount to be added to the account 
+
+const addMoneyInput = document.getElementById('input-add-money').value;
+
+
+// get the pin number
+const pinNumberInput = document.getElementById('input-pin-number').value;
+
+console.log(addMoneyInput,pinNumberInput);
+
+});
